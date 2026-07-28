05009e28c9bb573342d44b0abe2b6967f5ebc491
km-port <potepote1185@gmail.com>
2026-07-28 23:01:41 +0900

    Update cafe-summary-script.js

--- cafe-summary-script.js
+++ cafe-summary-script.js
@@ -2,7 +2,26 @@ window.addEventListener('load',()=>{
     loading.classList.add('loaded');
 })
 
+const zoom = document.querySelectorAll(".zoom");
+const zoomback = document.getElementById("zoomback");
+const zoomimg = document.getElementById("zoomimg");
 
+zoom.forEach(function(value)
+{
+    value.addEventListener("click",kakudai);
+});
+
+function kakudai(e){
+    zoomback.style.display
+    ="flex";
+    zoomimg.setAttribute("src",e.target.getAttribute("src"));
+}
+
+zoomback.addEventListener("click",modosu);
+function modosu(){
+    zoomback.style.display
+    ="none";
+}
 
 const triggers = document.querySelectorAll('.trigger');
 triggers.forEach(trigger =>{
@@ -16,4 +35,3 @@ triggers.forEach(trigger =>{
         });
     })
 });
-
